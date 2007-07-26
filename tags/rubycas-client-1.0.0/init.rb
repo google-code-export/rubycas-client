0ae143616b05958eb858063739d0c4721810cbfc
matt.zukowski <matt.zukowski@e5935e18-7a1f-0410-a9c8-d10ac5b774bc>
2007-07-26 13:33:27 +0000

    Snapshot of 1.0.0 release

---
+++ tags/rubycas-client-1.0.0/init.rb
@@ -0,0 +1,20 @@
+require 'cas_auth'
+require 'cas_logger'
+require 'cas_proxy_callback_controller'
+
+#CAS::Filter.logger = RAILS_DEFAULT_LOGGER if !RAILS_DEFAULT_LOGGER.nil?
+#CAS::Filter.logger = config.logger if !config.logger.nil?
+
+CAS::Filter.logger = CAS::Logger.new("#{RAILS_ROOT}/log/cas_client_#{RAILS_ENV}.log")
+CAS::Filter.logger.formatter = CAS::Logger::Formatter.new
+
+#if RAILS_ENV == "production"
+#  CAS::Filter.logger.level = Logger::WARN
+#else
+#  CAS::Filter.logger.level = Logger::DEBUG
+#end
+
+
+#class ActionController::Base
+#  append_before_filter CAS::Filter
+#end
